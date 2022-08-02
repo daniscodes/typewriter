96c193500bb5bafdf91e1468a157703515d8fbc3
daniscodes <dan97@bell.net>
2022-08-02 18:21:46 -0400

    Added \n at the end of sentence

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,8 @@
-const sentence = "hello this is your computer speaking";
+const sentence = "hello this is your computer speaking\n";
 let x = 0
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, x);
   x += 50
-
 }
